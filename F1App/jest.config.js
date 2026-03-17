953c595c85b12f0e9829d73a141dca198c5336ba
elias123 <eliastkurian2021@gmail.com>
2026-03-17 20:57:52 +0530

    fix: jest transformIgnorePatterns for ESM modules

--- F1App/jest.config.js
+++ F1App/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|lucide-react-native|react-native-svg|react-native-screens|react-native-safe-area-context|react-native-reanimated|react-native-worklets)/)',
+  ],
 };
